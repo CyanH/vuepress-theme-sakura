8bc67779da6e162eb58e80a2097ae3e8af2ef3a4
Cyan <chenhaiyanccc@163.com>
2019-07-18 17:32:50 +0800

    ''

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -19,28 +19,53 @@
         </span>
       </router-link>
     </div>
+    <div class="nav-background">
+      <div class="bgImg" :style="{backgroundImage:`url(${data.heroImage})`}"></div>
+    </div>
   </header>
 </template>
 
-<style lang="stylus" scoped>
-.navbar
-  position fixed
-  top 0
-  left 0
-  right 0
-  height $navbarHeight
-  background-color #fff
-  box-sizing border-box
+<script>
+export default {
+  computed: {
+    data () {
+      return this.$page.frontmatter
+    }
+  }
+}
+</script>
 
+<style lang="stylus" scoped>
 .nav-header
   position relative
+  color #fff
   padding 0 1rem
   margin 0 auto
   width 100%
   height $navbarHeight
   line-height $navbarHeight
+  z-index 1
 
 @media (max-width: $MQMobile)
   .home-link
     display none
+
+.nav-background
+  position absolute
+  width 100%
+  height 270px
+  top 0
+
+  .bgImg
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-repeat: repeat;
+    background-position: 50% 50%;
+    background-size: cover;
+    background-origin: padding-box;
+    background-attachment: scroll;
+    transition: all .45s;
 </style>
