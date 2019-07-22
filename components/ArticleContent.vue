2348f80d39e1cb74aec8a108c267933ccb05519f
Cyan <chenhaiyanccc@163.com>
2019-07-22 19:06:31 +0800

    '主体'

---
+++ components/ArticleContent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="site-content">
+    <div class="head">
+      <img
+        class="head-img"
+        :src="$themeConfig.logo"
+      >
+    </div>
+    <div class="content-area">
+        <main class="site-main" role="main">
+            <h1 class="main-title">
+                Discovery
+            </h1>
+            <slot />
+        </main>
+    </div>
+  </div>
+</template>
+
+
+<style lang="stylus" scoped>
+.site-content
+  position relative
+  animation main 1s
+  max-width 800px
+  padding 0 10px
+  margin 0 auto
+  z-index 3
+
+  .head
+    .head-img
+      border-radius 50%
+      width 100px
+      margin 0 auto
+      border 2px solid #fff
+      display block
+</style>
--- components/LayoutContainer.vue
+++ components/LayoutContainer.vue
@@ -2,8 +2,14 @@
   <div>
     <NavBar @toggle-sidebar="$emit('toggle-sidebar')" />
     <div class="container">
-      <ArticleCard />
+      <ArticleContent />
     </div>
     <MyFooter />
   </div>
 </template>
+
+<style lang="stylus" scoped>
+.container
+  @media (max-width: $MQMobile)
+    padding-top 220px
+</style>
