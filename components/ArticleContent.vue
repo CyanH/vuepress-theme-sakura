0b4d145fa1d8ccf85ed8ee0584083227e7a31c11
Cyan <chenhaiyanccc@163.com>
2019-07-23 19:37:06 +0800

    '主体提交'

--- components/ArticleContent.vue
+++ components/ArticleContent.vue
@@ -7,15 +7,55 @@
       >
     </div>
     <div class="content-area">
-        <main class="site-main" role="main">
-            <h1 class="main-title">
-                Discovery
-            </h1>
-            <slot />
-        </main>
+      <main
+        class="site-main"
+        role="main"
+      >
+        <h1 class="main-title">
+          Discovery
+        </h1>
+        <router-link
+          v-for="(item,index) in posts"
+          :key="index"
+        >
+          <article
+            class="post-list-thumb"
+            :class="{'post-list-thumb-left':index%2==0}"
+          >
+            <div class="post-thumb">{{item.name}}</div>
+            <div class="post-content-wrap" v-html="item.excerpt"></div>
+          </article>
+        </router-link>
+      </main>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  // 时间降序后的博客列表
+  computed: {
+    posts () {
+      let posts = this.$site.pages
+      posts = posts.filter(item => {
+        const { type, date } = item.frontmatter
+        if(type && type == 'post' && date){
+          return
+        }
+      })
+      posts.sort((a, b) => {
+        return this.getTimeNum(b) - this.getTimeNum(a)
+      })
+      return posts
+    }
+  },
+  methods:{
+    getTimeNum(date){
+      return parseInt(new Date(data.frontmatter.date).getTime())
+    }
+  }
+}
+</script>
 
 
 <style lang="stylus" scoped>
